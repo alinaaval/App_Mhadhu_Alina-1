43e6ffada82f13bd319f2824ac7d2bc9491cfdb6
mmhadhu <162150437+mmhadhu@users.noreply.github.com>
2024-05-22 18:33:38 +0200

    tagesplaner.py

--- tagesplaner.py
+++ tagesplaner.py
@@ -262,17 +262,17 @@ def main():
         else:
             st.write("Keine Termine für diesen Tag.")
 
- # Ändere die Hintergrundfarbe
-    st.markdown(
-        """
-        <style>
-        body {
-            background-color: #f0f2f6;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+# Ändere die Hintergrundfarbe auf Pastelrosa
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: #f5dcd6; /* Pastelrosa */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 if __name__ == "__main__":
     main()
