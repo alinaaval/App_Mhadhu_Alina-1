e9f8ef0d86a7a1a5ebf8d8585c1b9f13e21a62f2
alinaaval <162182957+alinaaval@users.noreply.github.com>
2024-05-22 20:02:23 +0200

    tagesplaner.py

--- tagesplaner.py
+++ tagesplaner.py
@@ -170,6 +170,9 @@ def main():
     if not st.session_state['authenticated']:
         st.title("Benutzerregistrierung und -anmeldung")
 
+# Bild hinzufügen
+        st.image("https://i0.wp.com/www.additudemag.com/wp-content/uploads/2018/03/For-Parents_DTPC-Motivation_bored-boy-at-desk_ts-866103068-cropped.jpeg", caption="Motivation", use_column_width=True)
+
         if st.checkbox("Registrieren"):
             # Benutzerregistrierung
             st.subheader("Registrierung")
