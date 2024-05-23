42b9c5f85212688ddadb3cafe0803133b82d5a9e
alinaaval <162182957+alinaaval@users.noreply.github.com>
2024-05-23 08:16:49 +0200

    tagesplaner.py

--- tagesplaner.py
+++ tagesplaner.py
@@ -199,16 +199,16 @@ def main():
                     st.error("Ungültige Anmeldeinformationen!")
         return
         
-# CSS für die Hintergrundfarbe direkt im Python-Code
-page_bg_img = '''
-<style>
-body {
-    background-color: #ADD8E6;  /* Hier kannst du jede gewünschte Farbe einstellen */
-}
-</style>
-'''
+    # CSS für die Hintergrundfarbe direkt im Python-Code
+    page_bg_img = '''
+    <style>
+    body {
+        background-color: #ADD8E6;  /* Hier kannst du jede gewünschte Farbe einstellen */
+    }
+    </style>
+    '''
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+    st.markdown(page_bg_img, unsafe_allow_html=True)
     
     st.title("To-do Liste")
 
