4a930da00cf70c64b0990bf93a3396584975e44f
alinaaval <162182957+alinaaval@users.noreply.github.com>
2024-05-23 08:23:53 +0200

    tagesplaner.py

--- tagesplaner.py
+++ tagesplaner.py
@@ -19,7 +19,7 @@ div[data-testid="stMarkdownContainer"] p {
     color: black;
 }
 h1, h2, h3, h4, h5, h6 {
-    color: black;
+    color: white;
 }
 .stButton > button {
     color: white !important;
